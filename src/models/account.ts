650fc286efb7be42342afa5b100f9170b9fe5ce5
Khang Vo <88926702+khangdzox@users.noreply.github.com>
2025-06-03 05:26:02 +1000

    feature/simplify account types

--- src/models/account.ts
+++ src/models/account.ts
@@ -1,14 +1,31 @@
-abstract class Account {
+import type { Cart } from "./cart";
+
+/**
+ * Represents a user account in the system.
+ */
+class Account {
   id: string;
   username: string;
   fullname: string;
   email: string;
   password: string;
+  role: string = 'customer';
+  address?: string;
+  phoneNumber?: string;
+  cart?: Cart;
 
+  /**
+   * Creates an instance of Account.
+   * @param id - Unique identifier for the account.
+   * @param username - Username for the account.
+   * @param fullname - Full name of the account holder.
+   * @param email - Email address associated with the account.
+   * @param password - Password for the account.
+   */
   constructor(id: string, username: string, fullname: string, email: string, password: string) {
     this.id = id;
     this.username = username;
-    this.fullname = fullname
+    this.fullname = fullname;
     this.email = email;
     this.password = password;
   }
--- src/models/admin.ts
+++
@@ -1,9 +0,0 @@
-import { Account } from './account';
-
-class Admin extends Account {
-  getRole() {
-    return 'admin';
-  }
-}
-
-export { Admin };
