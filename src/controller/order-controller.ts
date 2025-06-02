ae7ea74b9d62395d7c65558f9ac2fe4f06f0b5f2
Khang Vo <88926702+khangdzox@users.noreply.github.com>
2025-06-03 07:45:14 +1000

    feature/add order controller, work in progress

---
+++ src/controller/order-controller.ts
@@ -0,0 +1,128 @@
+import { Order } from "../models/order";
+import type { Product } from "../models/product";
+import type { JointPayment, Payment } from "../models/payment";
+import { Shipment, type JointShipment } from "../models/shipment";
+import { OrderRepository } from "../repository/order-repository";
+import { AccountController } from "./account-controller";
+import { CartController } from "./cart-controller";
+import { ShipmentController } from "./shipment-controller";
+import { PaymentController } from "./payment-controller";
+import { Receipt } from "../models/receipt";
+
+class OrderController {
+    private static _instance: OrderController;
+
+    private constructor() {}
+
+    static get instance(): OrderController {
+        if (!OrderController._instance) {
+            OrderController._instance = new OrderController();
+        }
+        return OrderController._instance;
+    }
+
+    async createOrder(
+        userId: string,
+        products: { product: Product; quantity: number }[],
+        payment: Payment,
+        shipment: Shipment
+    ): Promise<Order> {
+        const items = products.map(item => ({
+            productId: item.product.id,
+            quantity: item.quantity,
+        }));
+        const order = await OrderRepository.instance.create(userId, items, payment.id!, shipment.id!, "Pending", false);
+        return order;
+    }
+
+    async getOrderById(orderId: string): Promise<Order | null> {
+        return await OrderRepository.instance.getById(orderId);
+    }
+
+    async getOrdersByUser(userId: string): Promise<Order[]> {
+        return await OrderRepository.instance.getByUserId(userId);
+    }
+
+    async updateOrder(orderId: string, data: Partial<Order>): Promise<void> {
+        await OrderRepository.instance.update(orderId, data);
+    }
+
+    async deleteOrder(orderId: string): Promise<void> {
+        await OrderRepository.instance.delete(orderId);
+    }
+
+    async generateReceipt(order: Order): Promise<Receipt> {
+        if (!order.verify()) {
+            throw new Error("Order is not valid");
+        }
+
+        const user = await AccountController.instance.getAccount(order.userId);
+        if (!user) throw new Error(`User not found: ${order.userId}`);
+
+        const receipt = new Receipt(
+            order.id!,
+            user,
+            order.items.map(item => ({
+                product: item.product,
+                quantity: item.quantity,
+                price: item.product.price * item.quantity
+            })),
+            order.getTotalPrice(),
+            order.payment!,
+            order.shipment!
+        );
+
+        return receipt;
+    }
+
+    async checkout(paymentDetails: Partial<JointPayment>, shipmentDetails: Partial<JointShipment>): Promise<Order> {
+        if (!AccountController.loggedInUser) {
+            throw new Error("User not logged in");
+        }
+        if (!paymentDetails.type || !shipmentDetails.type) {
+            throw new Error("Payment and shipment type must be provided");
+        }
+
+        const user = await AccountController.instance.getAccount(AccountController.loggedInUser);
+        const cart = await CartController.instance.getCart(user.id);
+        if (cart.items.length === 0) {
+            throw new Error("Cart is empty");
+        }
+
+        const order = new Order(user.id);
+
+        cart.items.forEach(item => {
+            order.addItem(item.product, item.quantity);
+        });
+
+        if (shipmentDetails.type === "delivery") {
+            shipmentDetails = {
+                ...shipmentDetails,
+                partner: shipmentDetails.partner || "Australia Post",
+                address: shipmentDetails.address || user.address!
+            };
+        }
+
+        const shipment = ShipmentController.instance.createShipmentObject(shipmentDetails);
+
+        order.setShipment(shipment);
+
+        paymentDetails.amount = cart.getSubtotalPrice() + shipment.fee;
+
+        if (paymentDetails.type === "card") {
+            paymentDetails = {
+                ...paymentDetails,
+                cardNumber: paymentDetails.cardNumber || "",
+                expiryDate: paymentDetails.expiryDate || new Date().toISOString(),
+                paymentGateway: "",
+            }
+        }
+
+        const payment = PaymentController.instance.createPaymentObject(paymentDetails);
+        order.setPayment(payment);
+
+        return order;
+    }
+}
+
+export { OrderController };
