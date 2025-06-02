7ee9d543f88357ac0b1b7390649dd08b411e2971
Khang Vo <88926702+khangdzox@users.noreply.github.com>
2025-06-03 05:28:27 +1000

    feature/add category to product

--- src/models/product.ts
+++ src/models/product.ts
@@ -1,3 +1,5 @@
+import type { Category } from "./category";
+
 class Product {
     id: string;
     name: string;
@@ -5,22 +7,25 @@ class Product {
     shortDescription: string;
     longDescription: string;
     price: number;
+    category?: Category;
 
-    constructor(id: string, name: string, image:string, shortDescription: string, longDescription: string, price: number) {
+    constructor(id: string, name: string, image:string, shortDescription: string, longDescription: string, price: number, category?: Category) {
         this.id = id;
         this.name = name;
         this.image = image;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
         this.price = price;
+        this.category = category;
     }
 
-    updateDetails(name?: string, image?: string, shortDescription?: string, longDescription?: string, price?: number) {
+    updateDetails(name?: string, image?: string, shortDescription?: string, longDescription?: string, price?: number, category?: Category) {
         if (name) this.name = name;
         if (image) this.image = image;
         if (shortDescription) this.shortDescription = shortDescription;
         if (longDescription) this.longDescription = longDescription;
         if (price !== undefined) this.price = price;
+        if (category) this.category = category;
     }
 }
 
