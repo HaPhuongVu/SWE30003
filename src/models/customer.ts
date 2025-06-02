75255dfdc2710ee50e0803ef67e85310cdb1016a
Khang Vo <88926702+khangdzox@users.noreply.github.com>
2025-06-02 23:44:20 +1000

    update type for customer details

--- src/models/customer.ts
+++ src/models/customer.ts
@@ -1,17 +1,19 @@
 import { Account } from './account';
 import { Cart } from './cart';
 
+type CustomerDetails = {
+  personal: { phone: string | undefined };
+  delivery: { address: string | undefined };
+  payment: { cardNumber: string | undefined; expiryDate: string | undefined };
+};
+
 class Customer extends Account {
 
-  details: {
-    personal: { phone: string | undefined };
-    delivery: { address: string | undefined };
-    payment: { cardNumber: string | undefined; expiryDate: string | undefined };
-  };
+  details: CustomerDetails;
 
   shoppingCart: Cart;
 
-  constructor(id: string, username: string, fullname: string, email: string, password: string, details: any) {
+  constructor(id: string, username: string, fullname: string, email: string, password: string, details: CustomerDetails) {
     super(id, username, fullname, email, password);
     this.details = details;
     this.shoppingCart = new Cart(this.id);
