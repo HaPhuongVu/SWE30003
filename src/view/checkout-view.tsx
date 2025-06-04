5cd4e14f9c567fec2314704ce269e118a9bbdbd7
HaPhuongVu <161402592+HaPhuongVu@users.noreply.github.com>
2025-06-05 00:14:45 +1000

    add checkout view

---
+++ src/view/checkout-view.tsx
@@ -0,0 +1,138 @@
+import { Col, Container, ListGroup, Row } from "react-bootstrap";
+import { FormControl, FormLayout, FormSelect } from "../components/form";
+import { useQuery } from "@tanstack/react-query";
+import type { Cart } from "../models/cart";
+import { CartController } from "../controller/cart-controller";
+import { AccountController } from "../controller/account-controller";
+import Button from "../components/button";
+import { useState, type FormEvent } from "react";
+import { PaymentController } from "../controller/payment-controller";
+import { OrderController } from "../controller/order-controller";
+
+export default function CheckOutView() {
+  const loggedInUser = AccountController.loggedInUser;
+  const [address, setAddress] = useState('')
+  const [paymentMethod, setPaymentMethod] = useState('Cash')
+  const [shipmentMethod, setShipmentMethod] = useState('Pickup')
+  const [cardNumber, setCardNumber] = useState('')
+  const [errors, setErrors] = useState('')
+  const {data: cart, error, isLoading} = useQuery<Cart, Error>({
+    queryKey: ['cart', loggedInUser],
+    queryFn: () => {
+      if (!loggedInUser) throw new Error('User not logged in');
+      return CartController.instance.getCart(loggedInUser);
+    }
+  })
+  if (isLoading) return <div>Loading...</div>
+  if (error) throw error
+
+  async function handleSubmit(e: FormEvent<Element>) {
+    e.preventDefault();
+    let cardData = {
+      visaNumber: paymentMethod === "Visa" ? cardNumber : undefined,
+      creditNumber: paymentMethod === "Credit Card" ? cardNumber : undefined,
+      mastercardNumber: paymentMethod === "Mastercard" ? cardNumber : undefined
+    }
+
+    const newError = PaymentController.instance.validateForm(cardData)
+    const errorMsg = newError.visaNumber || newError.creditNumber || newError.mastercardNumber || "";
+    setErrors(errorMsg);
+    if (errorMsg) return;
+    try{
+      const paymentDetails = {
+        type: paymentMethod === "Cash" ? "cash" : "card",
+        ...(paymentMethod !== "Cash" && {
+          cardNumber,
+          expiryDate: new Date().toISOString(),
+          paymentGateway: "",
+        }),
+      }
+
+      const shipmentDetails = {
+        type: shipmentMethod.toLowerCase(),
+        status: "Pending",
+        address: shipmentMethod === "Delivery" ? address : undefined,
+        partner: shipmentMethod === "Delivery" ? "Australia Post" : undefined,
+        deliveryDate: shipmentMethod === "Delivery" ? new Date().toLocaleString('en') : undefined,
+      }
+      await OrderController.instance.checkout(paymentDetails, shipmentDetails)
+      alert("Order placed successfully!");
+    } catch (error) {
+      alert (error)
+    }
+  }
+  return (
+    <Container className="mt-5 vh-100">
+      <h2 className="fw-bold text-center">Checkout</h2>
+      <Row>
+        <Col className="col-6">
+          <FormLayout onSubmit={handleSubmit}>
+          <h4 className="fw-bold">Shipping Information</h4>
+            <Col className="col-12 mt-3">
+              <FormSelect onChange={(e: React.ChangeEvent<HTMLSelectElement>) => setShipmentMethod(e.target.value)}>
+                <option>Pickup</option>
+                <option>Delivery</option>
+              </FormSelect>
+            </Col>
+            {shipmentMethod !== 'Pickup' && (
+               <Col className="col-12 mt-3">
+               <FormControl
+               type='text'
+               placeholder='Enter your address'
+               value={address}
+               onChange={(e: React.ChangeEvent<HTMLInputElement>) => setAddress(e.target.value)}/>
+               </Col>
+            )}
+            <Col className="col-12 mt-3">
+            <FormControl type='text' placeholder='Contact number'/>
+            </Col>
+          <h4 className="fw-bold mt-4">Payment Details</h4>
+            <Col className="col-12 mt-3">
+              <FormSelect onChange={(e: React.ChangeEvent<HTMLSelectElement>) => setPaymentMethod(e.target.value)}>
+                <option>Cash</option>
+                <option>Credit Card</option>
+                <option>Visa</option>
+                <option>Mastercard</option>
+              </FormSelect>
+            </Col>
+            {paymentMethod !== 'Cash' && (
+              <>
+              <Col className="col-12 mt-3">
+                <FormControl value={cardNumber}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setCardNumber(e.target.value)}
+                placeholder='Card Number'/>
+                {errors && <div className="text-danger mt-1">{errors}</div>}
+              </Col>
+              <Row>
+              <Col className="col-6 mt-3">
+                <FormControl placeholder='MM/YY'/>
+              </Col>
+              <Col className="col-6 mt-3">
+                <FormControl placeholder='CVV'/>
+              </Col>
+              </Row>
+              </>
+            )}
+          <Button type='submit' variant="destructive" className="w-100 mt-3">Complete Order</Button>
+          <Button className="w-100 mt-3">Continue with shopping</Button>
+          </FormLayout>
+        </Col>
+        <Col className="col-6 mt-3">
+        <h4 className="fw-bold">Order</h4>
+          <ListGroup className="w-75">
+            {cart?.items.map((item) => (
+              <ListGroup.Item className="d-flex justify-content-between">
+                <span>{item.product.name} x {item.quantity}</span>
+                <span>${(item.product.price * item.quantity).toFixed(2)}</span>
+              </ListGroup.Item>
+            ))}
+          <ListGroup.Item className="d-flex justify-content-between fw-bold">
+            <span>Total</span>
+            <span>${CartController.instance.calculateTotal(cart!).toFixed(2)}</span>
+          </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
